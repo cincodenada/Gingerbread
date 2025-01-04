089800936cd7286ad7fb9265a539a9d3c79cbbf3
Ell Bradshaw <ell@cincodenada.com>
2025-01-04 15:08:26 -0800

    Add other two test files

--- web/scripts/tests/test_design.js
+++ web/scripts/tests/test_design.js
@@ -5,21 +5,23 @@ const { expect } = chai;
 
 describe('Exporter', function() {
     this.timeout(30000)
-    it('Should export the weasel design as expected', async function() {
-        const cvs = new PreviewCanvas(document.getElementById("preview-canvas"));
-        const image = await fetch('images/example-weasel.svg');
-        const svg_doc = new DOMParser().parseFromString(
-            await image.text(),
-            "image/svg+xml"
-        );
+    for(const name of ['weasel', 'speak', 'gear']) {
+        it(`Should export the ${name} design as expected`, async function() {
+            const cvs = new PreviewCanvas(document.getElementById("preview-canvas"));
+            const image = await fetch(`images/example-${name}.svg`);
+            const svg_doc = new DOMParser().parseFromString(
+                await image.text(),
+                "image/svg+xml"
+            );
 
-        console.log(svg_doc)
+            console.log(svg_doc)
 
-        const expectedPromise = fetch('scripts/tests/weasel.kicad_pcb').then(resp => resp.text())
-        const design = new Design(cvs, svg_doc);
-        const footprint = await design.generate_footprint();
-        console.log(footprint);
+            const expectedPromise = fetch(`scripts/tests/${name}.kicad_pcb`).then(resp => resp.text())
+            const design = new Design(cvs, svg_doc);
+            const footprint = await design.generate_footprint();
+            console.log(footprint);
 
-        expect(footprint).to.equal(await expectedPromise);
-    })
+            expect(footprint).to.equal(await expectedPromise);
+        })
+    }
 })
