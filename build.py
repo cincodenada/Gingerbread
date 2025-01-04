60d61e66728070520887c438886359c8eb65eeb5
Ell Bradshaw <ell@cincodenada.com>
2025-01-04 15:27:33 -0800

    Add build.py option to built tests page
    Default to not

--- build.py
+++ build.py
@@ -1,16 +1,34 @@
 import pathlib
 import jinja2
 import shutil
+import argparse
 
 try:
     import livereload
 except ImportError:
     livereload = None
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--tests', action="store_true")
+args = parser.parse_args()
+
 HERE = pathlib.Path(__file__).parent
 WEB = HERE / "web"
 NATIVE = HERE / "native"
 BUILD = HERE / "build"
+RESOURCES = [
+    "images",
+    "scripts",
+    "styles",
+    "favicon.ico",
+]
+EXCLUDE_PAGES = [
+    "test.html",
+]
+
+if(args.tests):
+    RESOURCES.append("scripts/tests")
+    EXCLUDE_PAGES = []
 
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(WEB),
@@ -23,7 +41,7 @@ def make_output_dirs():
 
 
 def copy_resources():
-    for name in ("images", "scripts", "scripts/tests", "styles", "favicon.ico"):
+    for name in (RESOURCES):
         src = WEB / name
         dest = BUILD / src.relative_to(WEB)
 
@@ -39,10 +57,14 @@ def build_pages():
     pages = WEB.glob("*.html")
     for page in pages:
         dest = BUILD / page.relative_to(WEB)
+        relpath = dest.relative_to(BUILD)
+        if str(relpath) in EXCLUDE_PAGES:
+            continue
+
         template = JINJA_ENV.get_template(str(page.relative_to(WEB)))
         rendered = template.render()
         dest.write_text(rendered)
-        print(f"Rendered {dest.relative_to(BUILD)}")
+        print(f"Rendered {relpath}")
 
 
 def copy_native():
