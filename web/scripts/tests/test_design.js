54f676925aef754aa73236801c40bb7066c21eba
Ell Bradshaw <ell@cincodenada.com>
2025-01-04 14:44:59 -0800

    Add test runner and skeleton of a test

---
+++ web/scripts/tests/test_design.js
@@ -0,0 +1,19 @@
+import { Design } from "../design.js";
+import { PreviewCanvas } from "../preview-canvas.js";
+
+describe('Exporter', function() {
+    this.timeout(30000)
+    it('Should export the weasel design as expected', async function() {
+        const cvs = new PreviewCanvas(document.getElementById("preview-canvas"));
+        const image = await fetch('images/example-weasel.svg');
+        const svg_doc = new DOMParser().parseFromString(
+            await image.text(),
+            "image/svg+xml"
+        );
+
+        const design = new Design(cvs, svg_doc);
+        await design.export('download');
+
+        assert(true);
+    })
+})
